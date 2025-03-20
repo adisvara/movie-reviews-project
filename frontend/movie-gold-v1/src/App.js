7053019f556cccf5f786910aea7fd12edcdb6b80
Adisvara <adisvara2022@gmail.com>
2025-03-20 18:25:12 +0530

    Configure for GitHub Pages deployment

--- frontend/movie-gold-v1/src/App.js
+++ frontend/movie-gold-v1/src/App.js
@@ -60,14 +60,13 @@ function App() {
     <div className="App">
       <Header/>
       <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route path="/" element={<Home movies={movies} />} ></Route>
-            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
-            <Route path="*" element = {<NotFound/>}></Route>
-          </Route>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies} />} ></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+          <Route path="*" element = {<NotFound/>}></Route>
+        </Route>
       </Routes>
-
     </div>
   );
 }
